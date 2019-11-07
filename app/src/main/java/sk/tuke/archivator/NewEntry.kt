1d75db8cc783902d806db1e54a9711a0c69d5df1
Lukas Josai <lukijosai@gmail.com>
2019-11-07 10:38:28 +0100

    Added room

--- app/src/main/java/sk/tuke/archivator/NewEntry.kt
+++ app/src/main/java/sk/tuke/archivator/NewEntry.kt
@@ -6,27 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
-import kotlinx.android.synthetic.main.fragment_main_screen.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class MainScreen : Fragment() {
+class NewEntry : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
-        val view = inflater.inflate(R.layout.fragment_main_screen, container, false)
-
-        view.button_newEntry.setOnClickListener{
-            view.findNavController().navigate(R.id.action_mainScreen_to_newEntry)
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_new_entry, container, false)
     }
 
 
